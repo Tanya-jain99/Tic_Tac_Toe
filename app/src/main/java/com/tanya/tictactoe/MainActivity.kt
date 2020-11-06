d2b74a9296fb3963342b06407fd8a7ba025e5ea3
Tanya-jain99 <jaintanya999@gmail.com>
2020-11-06 23:59:32 +0530

    ClickListeners added

--- app/src/main/java/com/tanya/tictactoe/MainActivity.kt
+++ app/src/main/java/com/tanya/tictactoe/MainActivity.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
      lateinit var board:Array<Array<Button>>
+     val PLAYER=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
